72a2810ac09cd2484148380e60f9ffd5b1165045
MaciejKubon <maciejk2233@gmail.com>
2024-03-14 22:55:49 +0100

    adding button support

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'CTP-APP';
+  ActiveLab: Number = 1;
+  ChangeActiveLab(value: number) {
+    this.ActiveLab = value;
+    console.log(this.ActiveLab);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { LeftNavbarComponent } from './left-navbar/left-navbar.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [AppComponent],
   providers: [],
   bootstrap: [AppComponent],
-  imports: [BrowserModule, LeftNavbarComponent],
+  imports: [BrowserModule, CommonModule, LeftNavbarComponent],
 })
 export class AppModule {}
