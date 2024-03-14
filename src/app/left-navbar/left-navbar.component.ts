ddf180c11f0ca4e0b2451b39181b4bc1a2fca394
MaciejKubon <maciejk2233@gmail.com>
2024-03-14 20:18:45 +0100

    create navbar

---
+++ src/app/left-navbar/left-navbar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-left-navbar',
+  standalone: true,
+  imports: [],
+  templateUrl: './left-navbar.component.html',
+  styleUrl: './left-navbar.component.css'
+})
+export class LeftNavbarComponent {
+
+}
